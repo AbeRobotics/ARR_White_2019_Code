2af57c23275cdeb0e8b0312431e92c5bb0882f94
evilpizza <kylestang.ks@gmail.com>
2018-12-15 01:21:15 -0700

    Added a lot of stuff to OPModeDriveHelper and OPModeCOnstants. Still much more work to be done, the code will not run in it's current state.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OPModeConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OPModeConstants.java
@@ -2,9 +2,35 @@ package org.firstinspires.ftc.teamcode;
 
 public class OPModeConstants {
 	
+	// TODO Need to update with actual value. See GitHub issue #10
+	public final double ticksPerInch = 0d;
+
+	// Auto Speeds, SLOW = 0.5; MEDIUM = 0.75; FAST = 1.0
 	public enum AutonomousSpeed{
 		SLOW,
 		MEDIUM,
 		HIGH
+	}
+	
+	private static OPModeConstants opModeConstants;
+	private AutonomousSpeed autoSpeed;
+	
+	private OPModeConstants() {
+		setAutoSpeed(AutonomousSpeed.HIGH);
+	}
+	
+	public static OPModeConstants getInstance() {
+        if(opModeConstants==null) {
+            opModeConstants = new OPModeConstants();
+        }
+        return opModeConstants;
+    }
+
+	public void setAutoSpeed(AutonomousSpeed speed) {
+		autoSpeed = speed;
+	}
+	
+	public AutonomousSpeed getAutoSpeed() {
+		return autoSpeed;
 	}
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OPModeDriveHelper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OPModeDriveHelper.java
@@ -3,19 +3,87 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class OPModeDriveHelper {
 	private static OPModeDriveHelper instance;
     private  OPModeConstants opModeConstants;
     private  Telemetry telemetry;
     private  HardwareMap hardwareMap;
-    private OPModeGyroHelper gyroHelper;
     DcMotor leftWheel;
     DcMotor rightWheel;
 
+    public static OPModeDriveHelper getInstance() {
+        if(instance==null) {
+            instance = new OPModeDriveHelper();
+        }
+        return instance;
+    }
+    
     public void Init(Telemetry telemetry, HardwareMap hardwareMap) {
     	this.telemetry = telemetry;
     	this.hardwareMap = hardwareMap;
         opModeConstants = OPModeConstants.getInstance();
     	
+        leftWheel = hardwareMap.dcMotor.get("left_wheel");
+        rightWheel = hardwareMap.dcMotor.get("right_wheel");
     }
+    
+    private OPModeDriveHelper(){}
+    
+    public boolean MoveForward(Double inches) {
+    	return MoveForward(inches, opModeConstants.getAutoSpeed());
+    }
+    
+    // TODO It seems redundant to copy MoveForward for MoveBackward. Will probably just create function that
+    // multiplies the input by -1 and calls MoveForward or create a single move function for MoveForward and MoveBackward.
+    public boolean MoveForward(Double inches, OPModeConstants.AutonomousSpeed speed) {
+    	ResetDriveEncoders();
+    	//SetForwardSteering();		TODO What is the point of this method? What advantage over SetAllStop()?
+    	double totalTicks = opModeConstants.ticksPerInch * inches;
+    	
+    	leftWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftWheel.setPower(GetPower(speed));
+        rightWheel.setPower(GetPower(speed));
+        leftWheel.setTargetPosition((int)totalTicks);
+        rightWheel.setTargetPosition((int)(totalTicks*-1));
+        while(leftWheel.isBusy()) {
+            telemetry.addData("Left Current Position -",leftWheel.getCurrentPosition());
+            telemetry.addData("Right Current Position -",rightWheel.getCurrentPosition());
+            telemetry.update();
+            sleep(10); // TODO What import do I use for this again?
+        }
+        SetAllStop();
+        ResetDriveEncoders();
+        return true;
+    }
+
+	private double GetPower(OPModeConstants.AutonomousSpeed speed) {
+		switch(speed){
+			case SLOW:
+				return 0.5;
+				break;
+			
+			case MEDIUM:
+				return 0.75;
+				break;
+			
+			case HIGH:
+				return 1.0;
+				break;
+		}
+	}
+
+	private void ResetDriveEncoders() {
+		leftWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+		rightWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+	}
+	
+    public void SetAllStop(){
+        leftWheel.setPower(0);
+        rightWheel.setPower(0);
+    }
+    
+    
 }
