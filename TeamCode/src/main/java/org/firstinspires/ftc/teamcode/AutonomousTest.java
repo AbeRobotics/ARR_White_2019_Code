d24effb59a4fab40736531ef6affd00c1522d44d
Thomas Spina <thomas@Chef.local>
2019-12-18 17:11:45 -0700

    made a test autonomous mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
@@ -16,31 +16,25 @@ public class AutonomousTest extends LinearOpMode {
     	OPModeDriveHelper driver = OPModeDriveHelper.getInstance();
 
     	driver.init(telemetry, hardwareMap, opModeConstants, this);
-    	
+
     	waitForStart();
 		telemetry.addData("Status: ", "Started");
 		telemetry.update();
 		sleep(1000);
     	runTime.reset();
-    	
-    	opModeConstants.setAutoSpeed(OPModeConstants.AutonomousSpeed.LOW);
-    	opModeConstants.setDriveDirection(OPModeConstants.DriveDirection.FORWARD);
-    	telemetry.addData("Runtime: ", runTime.toString());
-    	telemetry.update();
+
+
+		opModeConstants.setAutoSpeed(OPModeConstants.AutonomousSpeed.MEDIUM);
+		opModeConstants.setDriveDirection(OPModeConstants.DriveDirection.FORWARD);
+		driver.drive(20.0, opModeConstants.getAutoSpeed(), opModeConstants.getDriveDirection());
     	sleep(1000);
-    	driver.drive(24.0, opModeConstants.getAutoSpeed(), opModeConstants.getDriveDirection());
-    	sleep(2000);
-    	
-    	opModeConstants.setTurnDirection(OPModeConstants.TurnDirection.RIGHT);
-    	driver.driveTurn(opModeConstants.getTurnDirection(), opModeConstants.getAutoSpeed(), 360.0);
-    	sleep(2000);
-    	
-    	opModeConstants.setTurnDirection(OPModeConstants.TurnDirection.LEFT);
-    	opModeConstants.setAutoSpeed(OPModeConstants.AutonomousSpeed.HIGH);
-    	driver.driveTurn(opModeConstants.getTurnDirection(), opModeConstants.getAutoSpeed(), 360.0);
-    	sleep(2000);
-    	
-    	opModeConstants.setDriveDirection(OPModeConstants.DriveDirection.REVERSE);
-    	driver.drive(24.0, opModeConstants.getAutoSpeed(), opModeConstants.getDriveDirection());
+
+		opModeConstants.setTurnDirection(OPModeConstants.TurnDirection.RIGHT);
+		driver.driveTurn(opModeConstants.getTurnDirection(), opModeConstants.getAutoSpeed(), 90);
+		sleep(10000);
+
+		opModeConstants.setHorizontalDriveDirection(OPModeConstants.HorizontalDriveDirection.RIGHT);
+		driver.driveHorizontal(20.0, opModeConstants.getAutoSpeed(), opModeConstants.getHorizontalDriveDirection());
+		sleep(1000);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OPModeConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OPModeConstants.java
@@ -46,7 +46,6 @@ public class OPModeConstants {
 		RIGHT,
 		LEFT
 	}
-
 	
 	// Instance variables
 	
